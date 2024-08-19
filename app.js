64d479fa12583e17a8142b17d3821fed0b8d2cde
Elizabeth <liz4momentum@gmail.com>
2024-08-18 21:42:34 -0600

    Added content

--- app.js
+++ app.js
@@ -2,88 +2,121 @@
 let toggleNavStatus = false;
 
 let toggleNav = function () {
-    let getSidebar = document.querySelector(".navbar-menu");
-    let getSidebarUL = document.querySelector(".side-nav ul");
-    let getSidebarLinks = document.querySelectorAll(".side-nav a");
-    let getSidebarVisibility = document.querySelector(".side-nav");
-    var htmlGrab = document.querySelector("html");
-    const hamburger = document.querySelector('.hamburger');
+  let getSidebar = document.querySelector(".navbar-menu");
+  let getSidebarUL = document.querySelector(".side-nav ul");
+  let getSidebarLinks = document.querySelectorAll(".side-nav a");
+  let getSidebarVisibility = document.querySelector(".side-nav");
+  var htmlGrab = document.querySelector("html");
+  const hamburger = document.querySelector('.hamburger');
 
-    hamburger.classList.toggle('is-active');
+  hamburger.classList.toggle('is-active');
 
-    if (toggleNavStatus === false) {
+  if (toggleNavStatus === false) {
 
-      getSidebarVisibility.style.visibility = "visible";
-      getSidebarVisibility.style.pointerEvents = "initial";
+    getSidebarVisibility.style.visibility = "visible";
+    getSidebarVisibility.style.pointerEvents = "initial";
 
-      getSidebarLinks.forEach((item, index)=>{
-        console.log(item);
-        item.style.opacity = "1";
-        item.style.visibility = "visible";
-        });
-      getSidebar.style.width = "60%";
-      htmlGrab.classList.add("clicked");
-      toggleNavStatus = true;
-      servicesUL.classList.add("clicked");
-    } 
+    getSidebarLinks.forEach((item, index) => {
+      item.style.opacity = "1";
+      item.style.visibility = "visible";
+    });
+    getSidebar.style.width = "60%";
+    htmlGrab.classList.add("clicked");
+    toggleNavStatus = true;
+    servicesUL.classList.add("clicked");
+  }
 
-    else if (toggleNavStatus === true) {
+  else if (toggleNavStatus === true) {
 
-         getSidebarLinks.forEach((item, index)=>{
-          item.style.opacity = "0";
-          item.style.transitionDelay = "0s";
-          item.style.visibility = "hidden";
-         });
-        getSidebar.style.width = "0";
-        htmlGrab.classList.remove("clicked");
-        toggleNavStatus = false;
-        servicesUL.classList.remove("clicked");
-        getSidebarVisibility.style.pointerEvents = "none";
+    getSidebarLinks.forEach((item, index) => {
+      item.style.opacity = "0";
+      item.style.transitionDelay = "0s";
+      item.style.visibility = "hidden";
+    });
+    getSidebar.style.width = "0";
+    htmlGrab.classList.remove("clicked");
+    toggleNavStatus = false;
+    servicesUL.classList.remove("clicked");
+    getSidebarVisibility.style.pointerEvents = "none";
+  }
+}
+
+class Accordion {
+  constructor(el) {
+    this.el = el;
+    this.summary = el.querySelector('summary');
+    this.content = el.querySelector('.content');
+
+    this.animation = null;
+    this.isClosing = false;
+    this.isExpanding = false;
+    this.summary.addEventListener('click', (e) => this.onClick(e));
+  }
+  onClick(e) {
+    e.preventDefault();
+    this.el.style.overflow = 'hidden';
+    if (this.isClosing || !this.el.open) {
+      this.open();
+    } else if (this.isExpanding || this.el.open) {
+      this.shrink();
     }
+  }
+
+  shrink() {
+    this.isClosing = true;
+
+    const startHeight = `${this.el.offsetHeight}px`;
+    const endHeight = `${this.summary.offsetHeight}px`;
+
+    if (this.animation) {
+      this.animation.cancel();
+    }
+
+    this.animation = this.el.animate({
+      height: [startHeight, endHeight]
+    }, {
+      duration: 400,
+      easing: 'ease-out'
+    });
+
+    this.animation.onfinish = () => this.onAnimationFinish(false);
+    this.animation.oncancel = () => this.isClosing = false;
+  }
+
+  open() {
+    this.el.style.height = `${this.el.offsetHeight}px`;
+    this.el.open = true;
+    window.requestAnimationFrame(() => this.expand());
+  }
+
+  expand() {
+    this.isExpanding = true;
+    const startHeight = `${this.el.offsetHeight}px`;
+    const endHeight = `${this.summary.offsetHeight + this.content.offsetHeight}px`;
+
+    if (this.animation) {
+      this.animation.cancel();
+    }
+
+    this.animation = this.el.animate({
+      height: [startHeight, endHeight]
+    }, {
+      duration: 400,
+      easing: 'ease-out'
+    });
+    this.animation.onfinish = () => this.onAnimationFinish(true);
+    this.animation.oncancel = () => this.isExpanding = false;
+  }
+
+  onAnimationFinish(open) {
+    this.el.open = open;
+    this.animation = null;
+    this.isClosing = false;
+    this.isExpanding = false;
+    this.el.style.height = this.el.style.overflow = '';
+  }
 }
 
-
-// ------------------------------------------------- DARK MODE -----------------------------
-
-/* Body and Core Elements */
-var body = document.querySelector("body");
-
-// Dark Mode Action
-let darkMode = localStorage.getItem("darkMode");
-const darkModeToggle = document.querySelector('.dark-mode-button');
-// for an optional footer dark mode button as well
-const darkModeToggleFooter = document.querySelector('footer .dark-mode-button');
-
-// This is where you add the dakr mode class.  When the dark mode is enabled as true in localstorage,
-// it will add all the dark-mode classes to the elements we created in the variables above
-const enableDarkMode = () => {
-
-// Core dark mode styles
-body.classList.add("dark-mode");
-localStorage.setItem("darkMode", "enabled")
-}
-
-// This is where we remove dark mode.  Just copy and paste all the lines where you added a class
-// and paste them into this function, then change "addClass" to "removeClass"
-const disableDarkMode = () => {
-
-body.classList.remove("dark-mode");
-localStorage.setItem("darkMode", null)
-}
-
-if (darkMode == "enabled") {
-enableDarkMode();
-}
-
-// add event listener to the dark mode button toggle
-darkModeToggle.addEventListener('click', () => {
-// on click, check localstorage for the dark mode value
-darkMode = localStorage.getItem("darkMode");
-if (darkMode !== "enabled") {
-  // if dark mode is not enabled, run this function to set it to enabled
-  enableDarkMode();
-} else {
-  // if dark mode is enabled, run this function to set it to disabled
-  disableDarkMode();
-}
+document.querySelectorAll('details').forEach((el) => {
+  new Accordion(el);
 })
